47b0553307a28808c058cd36845184cbd5104657
SamsPC\Sam <samarseneault@yahoo.com>
2016-10-06 16:06:35 -0400

    Created a test QT project for the user interface. Later on this project will be integrated with visual studio, and will utilize the client-server system.

---
+++ UserInterface/UserInterface.pro
@@ -0,0 +1,19 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-10-06T15:52:54
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = UserInterface
+TEMPLATE = app
+
+
+SOURCES += main.cpp
+
+HEADERS  +=
+
+FORMS    +=
---
+++ UserInterface/mainwindow.cpp
@@ -0,0 +1,31 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QGraphicsView>
+
+/*
+ - QGraphicsScene
+ - QGraphicsItem - Abstract class which a new class is supposed to be derived from
+ - QGraphicsView - Widget used to visualize the scene. Prints the items on the screen.
+ */
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    //Creating a scene
+    QGraphicsScene * mainScene = new QGraphicsScene();
+
+    //Create an item to put into the scene
+    QGraphicsRectItem * rect = new QGraphicsRectItem();
+    rect->setRect(0,0,100,100); //X,Y, Width, Height
+
+    //Add item to the scene
+    mainScene->addItem(rect);
+
+
+    return a.exec();
+}
+
